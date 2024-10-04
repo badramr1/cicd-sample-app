c5ccbaf24bf2f7798b6527c4baa329e743354b83
Badr Amri <badr.amri@student.hogent.be>
2024-10-04 03:14:24 +0200

    img added to script

--- sample-app.sh
+++ sample-app.sh
@@ -4,16 +4,19 @@ set -euo pipefail
 mkdir tempdir
 mkdir tempdir/templates
 mkdir tempdir/static
+mkdir tempdir/img
 
 cp sample_app.py tempdir/.
 cp -r templates/* tempdir/templates/.
 cp -r static/* tempdir/static/.
+cp -r img/* tempdir/img/.
 
 cat > tempdir/Dockerfile << _EOF_
 FROM python
 RUN pip install flask
 COPY  ./static /home/myapp/static/
 COPY  ./templates /home/myapp/templates/
+COPY  ./img /home/myapp/img/
 COPY  sample_app.py /home/myapp/
 EXPOSE 5050
 CMD python /home/myapp/sample_app.py
